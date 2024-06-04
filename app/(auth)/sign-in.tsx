f17140e3be99edec4c6d0c24e327186823dc4f82
lqviet45 <lqviet455@gmail.com>
2024-06-04 08:34:02 +0700

    add signin

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ScrollView, Image, View, Text} from "react-native";
+import images from "@/constants/images";
+import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButton";
+import {Link} from "expo-router";
+
+const SignIn = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const submit = async () => {
+
+    }
+
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView>
+                <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+                    <Image
+                        source={images.logo}
+                        resizeMode='contain'
+                        className="w-[115px] h-[35px] self-center"
+                    />
+                    <Text className="text-2xl text-white
+          text-semibold mt-10 font-psemibold self-center">
+                        Login to Aura
+                    </Text>
+
+                    <FormField
+                        title='Email'
+                        value={form.email}
+                        handleChangeText={(e) => setForm({...form, email: e})}
+                        otherStyles='mt-7'
+                        keyBoardType='email-address'
+                    />
+
+                    <FormField
+                        title='Password'
+                        value={form.password}
+                        handleChangeText={(e) => setForm({...form, password: e})}
+                        otherStyles='mt-7'
+                        keyBoardType='password'
+                    />
+
+                    <CustomButton
+                        title='Sign In'
+                        handlePress={submit}
+                        containerStyles='mt-7'
+                        isLoading={isSubmitting}
+                    />
+
+                    <View className="justify-center pt-5 flex-row gap-2">
+                        <Text className="text-lg text-gray-100 font-pregular">
+                            Don't have an account?
+                        </Text>
+
+                        <Link href='/sign-up' className='text-lg font-psemibold text-secondary'>
+                            Sign up
+                        </Link>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default SignIn;
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
 import {SafeAreaView} from "react-native-safe-area-context";
 import {ScrollView, View, Image, Text} from "react-native";
 import images from "../constants/images";
+import CustomButton from "@/components/CustomButton";
+import {router} from "expo-router";
+import {StatusBar} from "expo-status-bar";
 
 const App = () => {
 
@@ -35,9 +38,20 @@ const App = () => {
                             resizeMode="contain"
                         />
                     </View>
+
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                        Where creativity meets innovation:
+                        embark on a journey of limitless exploration with Aora.
+                    </Text>
+
+                    <CustomButton
+                        title='Continue with Email'
+                        handlePress={() => router.push('sign-in')}
+                        containerStyles={'w-full mt-7'}
+                    />
                 </View>
             </ScrollView>
-
+            <StatusBar backgroundColor={'#161622'} style="light" />
         </SafeAreaView>
     );
 };
