1be93957cfb4488852718646d3e8c6cbcf8b3c25
Phan Nguyễn Hoàng Phương <pnhphuong.work1@gmail.com>
2024-06-27 11:37:52 +0700

    added: order detail screen

--- app/(order)/[id].tsx
+++ app/(order)/[id].tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import {router, useLocalSearchParams} from "expo-router";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {ScrollView} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {Booking} from "@/model/booking";
 import {CourtGroup} from "@/model/courtGroup";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const BookingDetail = () => {
     let {id} = useLocalSearchParams<{id: string}>();
@@ -33,10 +34,91 @@ const BookingDetail = () => {
 
 
     return (
-        <SafeAreaView>
-            <ScrollView className={"h-full"}>
-
-            </ScrollView>
+        <SafeAreaView className={"px-3"}>
+                <View className={"rounded-2xl bg-white row-col"}>
+                    <View className={"flex-row items-center m-2 px-5 py-2 border-amber-400 border-2 rounded-2xl"}>
+                        <Ionicons name={"alert-circle-outline"} size={56} color={"yellow"}/>
+                        <View className={"flex-col ml-4"}>
+                            <Text className={"text-lg mb-2 text-gray-500"}>
+                                Đã thanh toán
+                            </Text>
+                            <Text className={"font-pbold text-3xl"}>
+                                -200.000đ
+                            </Text>
+                        </View>
+                    </View>
+                    <View className={"row-col p-4"}>
+                        <View className={"flex-row justify-between mb-2"}>
+                            <Text className={"text-lg text-gray-500"}>
+                                Trạng thái
+                            </Text>
+                            <View className={"flex-row self-start px-2 py-0.5 bg-yellow-200 rounded-3xl "}>
+                                <Text className={"text-yellow-700 font-bold"}>Đang chờ</Text>
+                            </View>
+                        </View>
+                        <View className={"flex-row justify-between mb-2"}>
+                            <Text className={"text-lg text-gray-500"}>
+                                Thời gian
+                            </Text>
+                            <Text className={"text-lg"}>
+                                13:31 27/06/2024
+                            </Text>
+                        </View>
+                        <View className={"flex-row justify-between mb-2"}>
+                            <Text className={"text-lg text-gray-500"}>
+                                Mã giao dịch
+                            </Text>
+                            <View className={"w-[55%]"}>
+                                <Text className={"text-lg"}>
+                                    {id}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View className={"rounded-2xl my-2 p-4 bg-white row-col "}>
+                    <View className={"flex-row justify-between mb-2"}>
+                        <Text className={"text-lg text-gray-500"}>
+                            Dịch vụ
+                        </Text>
+                        <Text className={"text-lg"}>
+                            Đặt cọc sân
+                        </Text>
+                    </View>
+                    <View className={"flex-row justify-between mb-2"}>
+                        <Text className={"text-lg text-gray-500"}>
+                            Tên cửa hàng
+                        </Text>
+                        <Text className={"text-lg"}>
+                            Pickleball Cort Q.8
+                        </Text>
+                    </View>
+                    <View className={"flex-row justify-between mb-2"}>
+                        <Text className={"text-lg text-gray-500"}>
+                            Mô tả
+                        </Text>
+                        <View className={"w-[55%]"}>
+                            <Text className={"text-lg"}>
+                                Thanh toan dat coc ONLINE-FRI-06-27-2024-AWCGSW
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                {/*Buttons*/}
+                <View className={"mt-5"}>
+                    <View className={"flex-row justify-between px-2"}>
+                        <View className={"rounded-2xl px-5 py-1.5 w-[46%] items-center border-2 border-amber-300 bg-white"}>
+                            <Text className={"text-xl text-amber-300"}>
+                                Quay lại
+                            </Text>
+                        </View>
+                        <View className={"rounded-2xl px-3 py-1.5 w-[46%] items-center bg-green-500"}>
+                            <Text className={"text-xl text-white"}>
+                                Giao dịch mới
+                            </Text>
+                        </View>
+                    </View>
+                </View>
         </SafeAreaView>
     );
 };
